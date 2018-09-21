90be16c0b31e888d07c1e3e2bb9d910143fcea87
Mateus de Morais <mateusmorais78@gmail.com>
2018-09-21 19:44:09 -0300

    Rendimento bruto - Primeiro teste - barra vermelha

---
+++ src/tests/TstRendimentoBruto.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TstRendimentoBruto {
+	
+	@Test
+	public void testRendimentoBrutoUm() {
+		Aplicacao aplicacao = Aplicacao.criarAplicacao(60, 1000.00f, 8.5f);
+		
+		assertEquals(13.97, aplicacao.obterRendimentoBruto(), 0);
+	}
+}
