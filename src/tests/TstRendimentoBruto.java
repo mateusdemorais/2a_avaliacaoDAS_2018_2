371a2c3f8b5f70f27803c93a414c2e8fab3257fb
Mateus de Morais <mateusmorais78@gmail.com>
2018-09-21 20:04:15 -0300

    Rendimento bruto - Terceiro teste - barra verde (triangulados e parametrizados)

--- src/tests/TstRendimentoBruto.java
+++ src/tests/TstRendimentoBruto.java
@@ -2,23 +2,55 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.Aplicacao;
 
+@RunWith(Parameterized.class)
 public class TstRendimentoBruto {
 	
-	@Test
-	public void testRendimentoBrutoUm() {
-		Aplicacao aplicacao = Aplicacao.criarAplicacao(60, 1000.00f, 8.5f);
+	int tempo;
+	double aplicacaoInicial;
+	double taxaJurosAnual;
+	double rendimentoBruto;
+	
+	public TstRendimentoBruto(int tempo, double aplicacaoInicial, double taxaJurosAnual, double rendimentoBruto) {
+		this.tempo = tempo;
+		this.aplicacaoInicial = aplicacaoInicial;
+		this.taxaJurosAnual = taxaJurosAnual;
+		this.rendimentoBruto = rendimentoBruto;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters() {
 		
-		assertEquals(13.97, aplicacao.obterRendimentoBruto(), 0);
+		// Parâmetros 1
+		Object[] parametros1 = new Object[]{60, 1000.00f, 8.5f, 13.97};
+		
+		// Parâmetros 2
+		Object[] parametros2 = new Object[] {120, 500.00f, 8.0f, 13.15};
+		
+		// Parâmetros 3
+		Object[] parametros3 = new Object[] {240, 3000.00f, 9.0f, 177.53};
+		
+		return Arrays.asList(new Object[][] {
+			parametros1, 
+			parametros2, 
+			parametros3
+		});
 	}
 	
 	@Test
-	public void testRendimentoBrutoDois() {
-		Aplicacao aplicacao = Aplicacao.criarAplicacao(120, 500.00f, 8.0f);
+	public void testRendimentoBruto() {
+		Aplicacao aplicacao = Aplicacao.criarAplicacao(this.tempo, this.aplicacaoInicial, this.taxaJurosAnual);
 		
-		assertEquals(13.15, aplicacao.obterRendimentoBruto(), 0);
+		assertEquals(this.rendimentoBruto, aplicacao.obterRendimentoBruto(), 0);
 	}
+	
 }
