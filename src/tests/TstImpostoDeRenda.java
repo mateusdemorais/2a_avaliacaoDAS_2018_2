c3926e5d237a46ca5f8bd63a4974333b0a36a999
Mateus de Morais <mateusmorais78@gmail.com>
2018-09-21 20:27:04 -0300

    Imposto de Renda - Terceiro teste - barra verde (triangulados e parametrizados)

--- src/tests/TstImpostoDeRenda.java
+++ src/tests/TstImpostoDeRenda.java
@@ -2,24 +2,55 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.Aplicacao;
 
+@RunWith(Parameterized.class)
 public class TstImpostoDeRenda {
 
-	@Test
-	public void testImpostoDeRendaUm() {
-		Aplicacao aplicacao = Aplicacao.criarAplicacao(270, 2000.00f, 8.5f);
+	int tempo;
+	double aplicacaoInicial;
+	double taxaJurosAnual;
+	double impostoDeRenda;
+	
+	public TstImpostoDeRenda(int tempo, double aplicacaoInicial, double taxaJurosAnual, double impostoDeRenda) {
+		this.tempo = tempo;
+		this.aplicacaoInicial = aplicacaoInicial;
+		this.taxaJurosAnual = taxaJurosAnual;
+		this.impostoDeRenda = impostoDeRenda;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters() {
 		
-		assertEquals(25.15, aplicacao.obterImpostoDeRenda(), 0);
+		// Parâmetros 1
+		Object[] parametros1 = new Object[]{270, 2000.00f, 8.5f, 25.15};
+		
+		// Parâmetros 2
+		Object[] parametros2 = new Object[] {390, 100.00f, 7.5f, 1.40};
+		
+		// Parâmetros 3
+		Object[] parametros3 = new Object[] {1000, 100.00f, 7.5f, 3.08};
+		
+		return Arrays.asList(new Object[][] {
+			parametros1, 
+			parametros2, 
+			parametros3
+		});
 	}
 	
 	@Test
-	public void testImpostoDeRendaDois() {
-		Aplicacao aplicacao = Aplicacao.criarAplicacao(390, 100.00f, 7.5f);
+	public void testImpostoDeRenda() {
+		Aplicacao aplicacao = Aplicacao.criarAplicacao(this.tempo, this.aplicacaoInicial, this.taxaJurosAnual);
 		
-		assertEquals(1.40, aplicacao.obterImpostoDeRenda(), 0);
+		assertEquals(this.impostoDeRenda, aplicacao.obterImpostoDeRenda(), 0);
 	}
 	
 }
